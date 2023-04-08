b3c6d3c7cb2ff0ff67cc97e5cbe587bd541fc7e7
michalhorvath <michalhvt@gmail.com>
2023-04-08 15:19:34 +0200

    add basic design search

--- parmod-frontend/src/components/DesignsListPage/index.tsx
+++ parmod-frontend/src/components/DesignsListPage/index.tsx
@@ -1,12 +1,16 @@
 import { useState, useEffect } from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import { Link } from 'react-router-dom';
 
 import { DesignPreview } from '../../types';
 import designService from '../../services/designs';
 
 const DesignsListPage = () => {
+  const [filter, setFilter] = useState<string>('');
   const [designs, setDesigns] = useState<DesignPreview[]>([]);
 
   useEffect(() => {
@@ -17,10 +21,23 @@ const DesignsListPage = () => {
     void fetchDesignList();
   }, []);
 
+  const filteredDesigns = filter === '' ?
+    designs :
+    designs.filter(d => d.title.includes(filter));
+
   return (
     <Container>
+      <Form>
+        <Form.Group className="mb-3 mt-3" as={Row}>
+          <Form.Label column sm={1}>Search:</Form.Label>
+          <Col sm={11}>
+            <Form.Control type="text" placeholder="Search" 
+              value={filter} onChange={({ target }) => setFilter(target.value)}/>
+          </Col>
+        </Form.Group>
+      </Form>
       <ListGroup>
-        {designs.map(d => (
+        {filteredDesigns.map(d => (
           <ListGroup.Item key={d.id}>
             <Link to={`/designs/${d.id}`}>{d.title}</Link>
           </ListGroup.Item>
